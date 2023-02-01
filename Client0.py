bfe41540105807ef2501ffd0bb04351f1ff037b0
Vedant_Tarale <vedanttarale10@gmail.com>
2023-02-01 23:08:27 +0530

    exception handling

--- Client0.py
+++ Client0.py
@@ -3,16 +3,19 @@ import websockets
 import time
 
 async def send_data(websocket):
-    count = 0
-    initial_time = time.time()
+    # count = 0
+    # initial_time = time.time()
+    # while True:
+    #     while count%5 != 0:
+    #         count = count + 1
+    #         await asyncio.sleep(1)
+    #     if count == 5:
+    #         count = count + 1
+    #         await websocket.send(f"Received 5 Messages in {time.time()-initial_time}")
+    #         initial_time = time.time()
     while True:
-        while count%5 != 0:
-            count = count + 1
-            await asyncio.sleep(1)
-        if count == 5:
-            count = count + 1
-            await websocket.send(f"Received 5 Messages in {time.time()-initial_time}")
-            initial_time = time.time()
+        await websocket.send("hey Server")
+        await asyncio.sleep(1)
 
 
 async def receive_data(websocket):
@@ -26,10 +29,15 @@ async def receive_data(websocket):
             count = 0
 
 async def client():
-    async with websockets.connect("ws://localhost:8000") as websocket:
-        # Create tasks to send and receive messages concurrently
-        send_task = asyncio.create_task(send_data(websocket))
-        receive_task = asyncio.create_task(receive_data(websocket))
-        await asyncio.gather(send_task, receive_task)
+    while True:
+        try: 
+            async with websockets.connect("ws://localhost:8000") as websocket:
+                # Create tasks to send and receive messages concurrently
+                send_task = asyncio.create_task(send_data(websocket))
+                receive_task = asyncio.create_task(receive_data(websocket))
+                await asyncio.gather(send_task, receive_task)
+        except:
+            print("Server Not Responding, Attempting to reconnect...")
+            await asyncio.sleep(1)
 
 asyncio.run(client())
