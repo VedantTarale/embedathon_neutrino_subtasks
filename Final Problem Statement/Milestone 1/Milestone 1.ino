9372ec48fd18d69bbf201c06db1ae3493f031f89
Vedant Tarale <vedanttarale10@gmail.com>
2023-02-11 12:31:30 +0530

    updated milestone 1

--- Final Problem Statement/Milestone 1/Milestone 1.ino
+++ Final Problem Statement/Milestone 1/Milestone 1.ino
@@ -1,29 +1,36 @@
-#include <Wire.h>
-const int MPU = 0x68; // MPU6050 I2C address
-float AccX, AccY, AccZ;
-void setup() {
-  Serial.begin(115200);
-  Wire.begin();                      // Initialize comunication
-  Wire.beginTransmission(MPU);       // Start communication with MPU6050 // MPU=0x68
-  Wire.write(0x6B);                  // Talk to the register 6B
-  Wire.write(0x00);                  // Make reset - place a 0 into the 6B register
-  Wire.endTransmission(true);        //end the transmission
-  delay(20); //Change this delay to make values more readable
+#include<Wire.h>
+ 
+const int MPU = 0x68;
+int16_t AcX,AcY,AcZ;
+ 
+double x;
+double y;
+double z;
+ 
+void setup(){
+Wire.begin();
+Wire.beginTransmission(MPU);
+Wire.write(0x6B);
+Wire.write(0);
+Wire.endTransmission(true);
+Serial.begin(9600);
 }
-void loop() {
-  // === Read acceleromter data === //
-  Wire.beginTransmission(MPU);
-  Wire.write(0x3B); // Start with register 0x3B (ACCEL_XOUT_H)
-  Wire.endTransmission(false);
-  Wire.requestFrom(MPU, 6, true); // Read 6 registers total, each axis value is stored in 2 registers
-  //For a range of +-2g, we need to divide the raw values by 16384, according to the datasheet
-  AccX = (Wire.read() << 8 | Wire.read()) / 16384.0; // X-axis value
-  AccY = (Wire.read() << 8 | Wire.read()) / 16384.0; // Y-axis value
-  AccZ = (Wire.read() << 8 | Wire.read()) / 16384.0; // Z-axis value
-  Serial.print("Ax = ");
-  Serial.print(AccX);
-  Serial.print(", Ay = ");
-  Serial.print(AccY);
-  Serial.print(", Az = ");
-  Serial.println(AccZ);
+void loop(){
+Wire.beginTransmission(MPU);
+Wire.write(0x3B);
+Wire.endTransmission(false);
+Wire.requestFrom(MPU,6,true);
+AcX=Wire.read()<<8|Wire.read();
+AcY=Wire.read()<<8|Wire.read();
+AcZ=Wire.read()<<8|Wire.read();
+Wire.endTransmission(true); 
+Serial.print("AccX= ");
+Serial.print(AcX);
+ 
+Serial.print("\tAccY= ");
+Serial.print(AcY);
+ 
+Serial.print("\tAccZ= ");
+Serial.println(AcZ);
+delay(50);
 }
